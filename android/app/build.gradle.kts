942a5f52a385e0f4341334e0b6ff741caffa5f9b
tsuchida <yu41926t.tutida@icloud.com>
2025-11-28 16:27:00 +0900

    fix:地図を表示するように依存関係を追加

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,20 @@ if (envFile.exists()) {
     envProperties.load(FileInputStream(envFile))
 }
 
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
+}
+
 android {
     namespace = "com.example.future_seeds_app"
     compileSdk = flutter.compileSdkVersion
